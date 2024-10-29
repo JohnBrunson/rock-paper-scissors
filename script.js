e2a6c26ff081e7fdb4d583fce59158ff419ba571
JohnBrunson <JohnFBrunson@gmail.com>
2024-10-28 23:25:37 -0700

    Logic added. Will likely need a refactor.

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ let newGame = true;
 
 sessionStatistics = {
     player : "",
-    wins : 0,
-    losses : 0,
+    playerWins : 0,
+    playerLosses : 0,
     draw : 0,
     computerWins : 0,
-    computerLoss : 0,
-    computerDraw : 0,
+    computerLoss : 0
 
 }
 
@@ -52,11 +51,42 @@ function computerDecision () {
     return computerChoice;
 }
 
+function scoring () {
+    //TODO: ugly... should be refactored
+    // Make a logging function for this? Too much repeated code.
+    // Need to see if there's a way to simplify this with maybe a switch statement or combining conditions. For now, this should work.
+    // This needs a few more tests, but initial draft seems ok.
+    console.log(`Player choice is ${playerChoice} and Computer Choice is ${computerChoice}`);
+    if (playerChoice === computerChoice) {
+        sessionStatistics.draw++;
+        console.log(`Draw! Draw count is now ${sessionStatistics.draw}`)
+    }else if (playerChoice === 'r' && computerChoice === 's') {
+        sessionStatistics.playerWins++
+        console.log (`Player chose ${playerChoice} and computer chose ${computerChoice} Point awarded to Player. Score: ${sessionStatistics.playerWins}`);
+    }else if (playerChoice === 'r' && computerChoice === 'p'){
+        sessionStatistics.computerWins++
+        console.log (`Player chose ${playerChoice} and computer chose ${computerChoice} Point awarded to Player. Score: ${sessionStatistics.computerWins}`);
+    }else if (playerChoice === 'p' && computerChoice === 'r') {
+        sessionStatistics.playerWins++
+        console.log (`Player chose ${playerChoice} and computer chose ${computerChoice} Point awarded to Player. Score: ${sessionStatistics.playerWins}`);
+    }else if (playerChoice ==='p' && computerChoice === 's' ) {
+        sessionStatistics.computerWins++
+        console.log (`Player chose ${playerChoice} and computer chose ${computerChoice} Point awarded to Player. Score: ${sessionStatistics.computerWins}`);
+    }else if (playerChoice === 's' && computerChoice === 'r') {
+        sessionStatistics.computerWins++
+        console.log (`Player chose ${playerChoice} and computer chose ${computerChoice} Point awarded to Player. Score: ${sessionStatistics.computerWins}`);
+    }else if (playerChoice === 's' && computerChoice === 'p') {
+        sessionStatistics.playerWins++
+        console.log (`Player chose ${playerChoice} and computer chose ${computerChoice} Point awarded to Player. Score: ${sessionStatistics.playerWins}`);
+    }
+
+};
 // Main gameplay loop
 while (playAgain === true){
     //made into functions for ease of troubleshooting/isolating logic.
     start();
     playerDecision();
     computerDecision();
+    scoring();
     playAgain = confirm('Do you wish to play again?');
     };
